e4b9c766b713fa352e67f3b4445e0e9a71763b65
Krasyo <doanusho@gmail.com>
2024-02-12 19:55:09 +0000

    Add dependencies, classes and moves

---
+++ moves.js
@@ -0,0 +1,49 @@
+const eeveeMoves = {
+  "Tackle" : [18, 1],
+  "Headbutt" : [23, 2],
+  "Tail whip" : [27, 3]
+}
+
+const flareonMoves = {
+  "Tackle" : [18, 1],
+  "Fire blast" : [23, 2],
+  "Fire spin" : [27, 3]
+}
+
+const vaporeonMoves = {
+  "Tackle" : [18, 1],
+  "Water pulse" : [23, 2],
+  "Hydro pump" : [27, 3]
+}
+
+const leafeonMoves = {
+  "Tackle" : [18, 1],
+  "Gigadrain" : [23, 2],
+  "Magical leaf" : [27, 3]
+}
+
+const charmanderMoves = {
+  "Tackle" : [18, 1],
+  "Flamethrower" : [23, 2],
+  "Fire fang" : [27, 3]
+}
+
+const squirtleMoves = {
+  "Tackle" : [18, 1],
+  "Surf" : [23, 2],
+  "Water gun" : [27, 3]
+}
+
+const bulbasaurMoves = {
+  "Tackle" : [18, 1],
+  "Razor leaf" : [23, 2],
+  "Seed bomb" : [27, 3]
+}
+
+const meowthMoves = {
+  "Tackle" : [18, 1],
+  "Scratch" : [23, 2],
+  "Death purr" : [27, 3]
+}
+
+module.exports = {eeveeMoves, flareonMoves, vaporeonMoves, leafeonMoves, charmanderMoves, squirtleMoves, bulbasaurMoves, meowthMoves}
